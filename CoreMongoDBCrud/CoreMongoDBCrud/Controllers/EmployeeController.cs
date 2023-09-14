e662aed7148d9bd60fa8ec7c274a1fdc56681487
parthk219 <129992000+parthk219@users.noreply.github.com>
2023-09-15 00:07:28 +0530

    C2

---
+++ CoreMongoDBCrud/CoreMongoDBCrud/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using CoreMongoDBCrud.IReopsitory;
+using CoreMongoDBCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMongoDBCrud.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private IEmployeeRepository _empRepo = null; // pvt field 
+        public EmployeeController(IEmployeeRepository empRepo) { _empRepo = empRepo; }  // IEmployeeRepository constructor taking argument 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEmpolyees()
+        {
+            var Employees = _empRepo.Gets();   // gets() inside _empRepo which is variable of IEmployeeRepository
+            return Json(Employees);
+        }
+        
+        public JsonResult SaveEmp(Employee employee)
+        {
+            var emp = _empRepo.Save(employee);
+            return Json(emp);
+        }
+
+        public JsonResult DeleteEmp(string employeeId)
+        {
+            var deletedEmployee = _empRepo.Delete(employeeId);
+            return Json(deletedEmployee);
+        }
+
+    }
+}
